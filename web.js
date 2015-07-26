1c01db59d3c703566a80b6ccf6d5b72bc02427c4
Leonardo Moura <leofsmoura@gmail.com>
2015-07-26 20:06:11 -0300

    fix backend and clean it

--- web.js
+++ web.js
@@ -1,31 +1,22 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var fs = require('fs');
 var pg = require('pg');
 
 var app = express();
 app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
-//app.use(express.static('js'));
-app.use(express.static(__dirname + '/css'));
 
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/chat';
 
-var respondGetMessage = function (response) {
-  var messages = fs.readFileSync('chat.txt', 'utf-8').split('\n');
-  if (messages.length-1 > id) {
-    response.send(messages[id]);
-  } else {
-    // TODO: check if user is out
-    setTimeout(function() { respondGetMessage(response); }, 1);
-  }
-};
-
 var stripHtml = function(s) {
   return s.replace(/<\/?[^>]+(>|$)/g, "");
-}
+};
 
+
+app.get('/', function(request, response) {
+  response.send(fs.readFileSync('index.html').toString());
+});
 
 app.post('/messages/:last_id', function(request, response) {
   console.log('request sent' + request.body.message);
@@ -34,26 +25,27 @@ app.post('/messages/:last_id', function(request, response) {
       // Handle Errors
       if(err) {
         console.log(err);
+        return response.sendStatus(401);
       }
 
       // SQL Query > Insert Data
       client.query('INSERT INTO messages(username, user_id, message, date) values($1, $2, $3, $4)', [msg.username, msg.user_id, msg.message, new Date().getTime()]).on('end', function() {
         response.sendStatus(200);
       });
-  });
-});
 
-app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html').toString());
+      done();
+  });
 });
 
 app.get('/messages/:last_id', function(request, response) {
   var last_id = request.params.last_id;
   console.log("user asked for a message " + last_id);
   pg.connect(connectionString, function(err, client, done) {
-    if(err)
+    if(err) {
       console.log(err);
-
+      done();
+      return response.sendStatus(401);
+    }
     var messages = [];
     var query = client.query('SELECT * FROM messages WHERE id > $1 ORDER BY date ASC', [last_id]);
     query.on('row', function(row) {
@@ -62,10 +54,12 @@ app.get('/messages/:last_id', function(request, response) {
 
     query.on('end', function() {
       client.end();
-      if (messages.length > 0)
+      if (messages.length > 0) {
         response.send(messages);
-      else
+      } else {
         response.sendStatus(304);
+      }
+      done();
     });
   });
 });
