f8244dd9a7088da0bc7522f49c7de62cf798472f
Leonardo Moura <leofsmoura@gmail.com>
2015-07-26 22:21:15 -0300

    chat in angular

--- public/angular/js/main.js
+++ public/angular/js/main.js
@@ -1,20 +1,49 @@
-angular.module('chatApp', [])
-    //.factory('Message', function($resource) {
-    //    return $resource('/messages/:id')
-   // })
+angular.module('chatApp', ['ngResource'])
+    .controller('ChatController', function($resource){
+        var Message = $resource('/messages/:msgId',
+            { msgId:'0' }, {
+                getLast: {method: 'GET', isArray: true}
+            });
 
-    .controller('ChatController', function(){
         var chat = this;
-        chat.user = 'anonymous'
+        chat.userId = new Date().getTime();
+        chat.username = 'anonymous'
         chat.changeuserdisabled = '';
+        chat.lastId = 0;
         chat.msgs = [];
+        chat.msgs = Message.query(function(){
+            chat.lastId = _.max(_.pluck(chat.msgs, 'id'));
+            if ($('#auto-scroll').is(':checked')) {
+                $('#room').animate({scrollTop: chat.msgs.length * 60}, 1000);
+            }
+
+            setInterval(function() {
+                var ret = Message.getLast({msgId: chat.lastId}, function(){
+                    if (ret.length) {
+                        chat.lastId = _.max(_.pluck(ret, 'id'));
+                        _.forEach(ret, function(msg) {
+                           chat.msgs.push(msg);
+                        });
+                        if ($('#auto-scroll').is(':checked')) {
+                            $('#room').animate({scrollTop: chat.msgs.length * 60}, 1000);
+                        }
+                    }
+                });
+            }, 5000);
+        });
 
         chat.sendMsg = function() {
-            if (chat.text.length <= 0) {
+            if (chat.message.length <= 0) {
                 return;
             }
+
+            var msg = new Message({
+                user_id: chat.userId,
+                username: chat.username,
+                message: chat.message });
+            msg.$save();
+
             $('#user').prop('disabled', true);
-            chat.msgs.push({user: chat.user, text: chat.text });
-            chat.text = ''
+            chat.message = ''
         };
     });
